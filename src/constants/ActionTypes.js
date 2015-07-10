37a54fd89f0744f60fccdb2b5eec3d97df2d32f9
C.T. Lin <chentsulin@gmail.com>
2015-07-10 12:26:59 +0800

    remove comment

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,9 +1,8 @@
 import create from 'create-action-types';
 
-// can be improve with keymirror
 const types = [
   'INCREMENT_COUNTER',
   'DECREMENT_COUNTER',
-]
+];
 
 export default create(types);
